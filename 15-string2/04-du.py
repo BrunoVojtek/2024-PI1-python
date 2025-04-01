6c225593a690683d8a226b07fcaa76ec66bb022e
BrunoVojtek <vojtek.bruno22@gmail.com>
2025-04-01 19:00:46 +0200

    finish_du.py

--- 15-string2/04-du.py
+++ 15-string2/04-du.py
@@ -1,11 +1,18 @@
-# uzivatel zada celu vetu a program vetu rozdeli na slova, ktore vypise nahodnou farbou na nahodnom mieste
-# aj pismenka zo slov vypise nahodnymi farbami
 import tkinter as tk
 import random
 
-text = input("Zadaj vetu: ")
+veta = input("Zadaj vetu: ")
+slova = veta.split()
 
+c = tk.Canvas(width=800, height=600)
+c.pack()
 
-
+for slovo in slova:
+    x = random.randint(10, 700)
+    y = random.randint(10, 500)
+    for pismeno in slovo:
+        farba_pismena = f'#{random.randint(0, 255):02x}{random.randint(0, 255):02x}{random.randint(0, 255):02x}'
+        c.create_text(x, y, fill=farba_pismena, text=pismeno)
+        x += 20  
 
 tk.mainloop()
