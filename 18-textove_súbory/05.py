018e443ffc69913edb2b0422f7910ff7ca9b1325
BrunoVojtek <vojtek.bruno22@gmail.com>
2025-04-23 19:23:33 +0200

    finishing homework

--- 18-textove_súbory/05.py
+++ 18-textove_súbory/05.py
@@ -1,19 +1,25 @@
 import tkinter as tk
 
-c = tk.Canvas()
+root = tk.Tk()
+c = tk.Canvas(root, width=400, height=400)
 c.pack()
 
-fbody = open ("body.txt", "r")
-for body in fbody:
-    print(body)
-    medzera = body.find(" ")
-    print(medzera)
-    x = int(body[:medzera])
-    y = int(body[medzera+1:])
-    print(x)
-    print(y)
-    c.create_oval(x-5,y-5,x+5,y+5)
+with open("body.txt", "r") as fbody:
+    riadky = fbody.readlines()
+
+for i in range(0, len(riadky), 3):
+    obrys = riadky[i].strip()
+    farba = riadky[i+1].strip()
+    pozícia = riadky[i+2].strip()
+
+    medzera = pozícia.find(" ")
+    x = int(pozícia[:medzera])
+    y = int(pozícia[medzera+1:])
+
+    if obrys == "o":
+        c.create_oval(x - 5, y - 5, x + 5, y + 5, fill=farba)
+    elif obrys == "r":
+        c.create_rectangle(x - 5, y - 5, x + 5, y + 5, fill=farba)
 
 
-fbody.close()
-tk.mainloop()
+root.mainloop()
